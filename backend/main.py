fbd8aab18d445e9d3bc981c4c5a6ef74d6b72de3
Yokesh <yokeshbaskaran@gmail.com>
2026-05-05 19:13:45 +0530

    added "venv" in .gitignore and added routes in main file

--- backend/main.py
+++ backend/main.py
@@ -16,19 +16,19 @@ app.add_middleware(
 todos = []
 
 
-# @app.get("/")
-# def home():
-#     return {"message": "Backend working!"}
+@app.get("/")
+def home():
+    return {"message": "Backend working!"}
 
 
 @app.get("/todos")
 def get_todos():
-    print("todos:", todos)
+    # print("todos:", todos)
     return todos
 
 
 @app.post("/todos")
 def add_todo(todo: dict):
     todos.append(todo)
-    print("todos:", todos)
+    # print("todos:", todos)
     return {"message": "Todo added"}
